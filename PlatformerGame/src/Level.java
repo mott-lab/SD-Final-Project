b5b7bfc76ff7f1738ea3c10200d52830dcaac45f
mgottsacker34 <mgottsacker34@gmail.com>
2016-12-10 14:52:01 -0600

    Working 12.10 14:51

--- PlatformerGame/src/Level.java
+++ PlatformerGame/src/Level.java
@@ -30,63 +30,65 @@ public class Level {
 		
 		if (level == 1)
 		{
-		Platform firstplat = new Platform(1, 540, 20);
-		platforms.add(firstplat);
-		Platform secondplat = new Platform(1325, 540);
-		platforms.add(secondplat);
-
-		int farX = 1300;
-		
-		for(int i = 0; i < 50; i++){
-//			Platform platform = new Platform(1300, 320);
-			if (i>1)
-			{
-				System.out.println(i);
-				Platform oldplat = (Platform) platforms.get((i-1));
-//				platform.setX(1300+(64*oldplat.getNumBlocks())+25);
-				int newX = farX + (64*oldplat.getNumBlocks());
-				int newY;
-				if (oldplat.getY()<90)
+			Platform firstplat = new Platform(1, 540, 20);
+			platforms.add(firstplat);
+			Platform secondplat = new Platform(1325, 540);
+			platforms.add(secondplat);
+	
+			int farX = 1300;
+			
+			for(int i = 0; i < 50; i++){
+	//			Platform platform = new Platform(1300, 320);
+				if (i>1)
 				{
-//					platform.setY(oldplat.getY()-60);
-					newY = oldplat.getY() - 60;
-				} else if (oldplat.getY()>570) {
-//					platform.setY(oldplat.getY()+60);
-					newY = oldplat.getY()+60;
-				} else {
-					int diff = (int) (Math.random()*50 - 50);
-//					platform.setY(oldplat.getY()+diff);
-					newY = oldplat.getY() + diff;
+					System.out.println(i);
+					Platform oldplat = (Platform) platforms.get((i-1));
+	//				platform.setX(1300+(64*oldplat.getNumBlocks())+25);
+					int newX = farX + (64*oldplat.getNumBlocks());
+					int newY;
+					if (oldplat.getY()<90)
+					{
+	//					platform.setY(oldplat.getY()-60);
+						newY = oldplat.getY() - 60;
+					} else if (oldplat.getY()>570) {
+	//					platform.setY(oldplat.getY()+60);
+						newY = oldplat.getY()+60;
+					} else {
+						int diff = (int) (Math.random()*50 - 50);
+	//					platform.setY(oldplat.getY()+diff);
+						newY = oldplat.getY() + diff;
+					}
+					Platform platter = new Platform(newX, newY);
+					platforms.add(platter);
+					
+					farX += (64*oldplat.getNumBlocks())+25;
+					
+	//				platform.setOriginx(newX);
+	//				platform.setOriginy(newY);
+	//				
+	//				Platform newplat = new Platform(newX, newY);
+	//				platforms.add(platform);
+					
+	//				if (platform.see == true) {
+	//					g2.drawImage(platform.img, platform.x, platform.y, null);
+	//					g2.draw(platform.getCollisionBox());
+	//				}
+					
+				} else{
+					Platform newplat = new Platform(1300,540);
+					newplat.setSee(true);
+					platforms.add(newplat);
+	//				if (platform.see == true) {
+	//					g2.drawImage(platform.img, platform.x, platform.y, null);
+	//					g2.draw(platform.getCollisionBox());
+	//				}
 				}
-				Platform platter = new Platform(newX, newY);
-				platforms.add(platter);
 				
-				farX += (64*oldplat.getNumBlocks())+25;
-				
-//				platform.setOriginx(newX);
-//				platform.setOriginy(newY);
-//				
-//				Platform newplat = new Platform(newX, newY);
-//				platforms.add(platform);
-				
-//				if (platform.see == true) {
-//					g2.drawImage(platform.img, platform.x, platform.y, null);
-//					g2.draw(platform.getCollisionBox());
-//				}
-				
-			} else{
-				Platform newplat = new Platform(1300,540);
-				newplat.setSee(true);
-				platforms.add(newplat);
-//				if (platform.see == true) {
-//					g2.drawImage(platform.img, platform.x, platform.y, null);
-//					g2.draw(platform.getCollisionBox());
-//				}
 			}
 			
+			//add goal platform
 		}
-		}
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public ArrayList<Platform> getPlatforms() {
